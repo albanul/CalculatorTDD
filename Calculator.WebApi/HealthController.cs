7e23d874ed812011528bc91de6907fe21eaa5f53
Alex Banul <aleksandr.banul@avanade.com>
2021-02-28 23:03:16 +0200

    Implement HealthController. Test is now green

---
+++ Calculator.WebApi/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.WebApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
