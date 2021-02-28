2d05c06f1c5940365d24cacf70f7e0674aea86d5
Alex Banul <albanul92@gmail.com>
2021-02-28 22:58:29 +0200

    Create first test to check if server is accessible

---
+++ Calculator.WebApiTests/HealthControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace CalculatorPOC
+{
+    public class HealthControllerTests
+    {
+        [Test]
+        public async Task Get_ShouldReturnOk()
+        {
+            // assert
+            var factory = new WebApplicationFactory<Startup>();
+            HttpClient client = factory.CreateClient();
+
+            // act
+            HttpResponseMessage response = await client.GetAsync("/health");
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
