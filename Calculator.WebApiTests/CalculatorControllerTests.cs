4859deb71220ff84cf3b5bbaabf851eedcbda08f
Alex Banul <aleksandr.banul@avanade.com>
2021-02-28 23:18:43 +0200

    Add red test that 1 + 1 equal 2

--- Calculator.WebApiTests/CalculatorControllerTests.cs
+++ Calculator.WebApiTests/CalculatorControllerTests.cs
@@ -22,5 +22,26 @@ namespace CalculatorPOC
             // assert
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
+
+        [Test]
+        public async Task GetAdd_ShouldReturnCorrectValue()
+        {
+            // assert
+            var factory = new WebApplicationFactory<Startup>();
+            HttpClient client = factory.CreateClient();
+
+            // act
+            HttpResponseMessage response = await client.GetAsync("/add?a=1&b=1");
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            string content = await response.Content.ReadAsStringAsync();
+            Assert.That(content, Is.Not.Null);
+            Assert.That(content, Is.Not.Empty);
+
+            int actual = int.Parse(content);
+            Assert.That(actual, Is.EqualTo(2));
+        }
     }
 }
