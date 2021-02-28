a10265d1edee2e71ba6cf70893f69d5f8950fd09
Alex Banul <aleksandr.banul@avanade.com>
2021-02-28 23:24:43 +0200

    Do tests refactoring

--- Calculator.WebApiTests/CalculatorControllerTests.cs
+++ Calculator.WebApiTests/CalculatorControllerTests.cs
@@ -9,15 +9,20 @@ namespace CalculatorPOC
 {
     public class CalculatorControllerTests
     {
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            _client = factory.CreateClient();
+        }
+
         [Test]
         public async Task GetAdd_ShouldReturnOk()
         {
-            // arrange
-            var factory = new WebApplicationFactory<Startup>();
-            HttpClient client = factory.CreateClient();
-
             // act
-            HttpResponseMessage response = await client.GetAsync("/add?a=1&b=1");
+            HttpResponseMessage response = await _client.GetAsync("/add?a=1&b=1");
 
             // assert
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -28,12 +33,8 @@ namespace CalculatorPOC
         [TestCase(1, -2, -1)]
         public async Task GetAdd_ShouldReturnCorrectValue(int a, int b, int expected)
         {
-            // assert
-            var factory = new WebApplicationFactory<Startup>();
-            HttpClient client = factory.CreateClient();
-
             // act
-            HttpResponseMessage response = await client.GetAsync($"/add?a={a}&b={b}");
+            HttpResponseMessage response = await _client.GetAsync($"/add?a={a}&b={b}");
 
             // assert
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
