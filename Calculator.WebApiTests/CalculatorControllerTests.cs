aec68549d870c37f4bec397d73c752e8117120be
Alex Banul <aleksandr.banul@avanade.com>
2021-02-28 23:10:54 +0200

    Add red test that /add endpoint is accessible

---
+++ Calculator.WebApiTests/CalculatorControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Calculator.WebApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace CalculatorPOC
+{
+    public class CalculatorControllerTests
+    {
+        [Test]
+        public async Task GetAdd_ShouldReturnOk()
+        {
+            // arrange
+            var factory = new WebApplicationFactory<Startup>();
+            HttpClient client = factory.CreateClient();
+
+            // act
+            HttpResponseMessage response = await client.GetAsync("/add?a=1&b=1");
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
